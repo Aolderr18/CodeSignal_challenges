dec8fdc607cad9c4906d3ef0ecf4b59f07052d4e
Aolderr18 <Aolderr18@gmail.com>
2019-04-06 14:18:57 -0500

    Update powersOfTwo.py

--- powersOfTwo.py
+++ powersOfTwo.py
@@ -1,12 +1,16 @@
+'''
+Every positive integer can be uniquely represented as a sum of different positive powers of two.
+
+Given a number n, return a sorted array of different powers of two that sum up to n
+'''
 def powersOfTwo(n):
     powers = []
-    dest = n 
-    while dest > 0:
+    while n > 0:
         p = 0
-        while 2**p <= dest:
+        while 2**p <= n:
             p = p + 1
         p = p - 1 
         powers.append(2**p)
-        dest = dest - 2**p 
+        n = n - 2**p 
     powers.reverse()
     return powers;
